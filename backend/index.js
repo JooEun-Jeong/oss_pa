0039443fd665b562b786b3dbf848d3197436026a
JooEun-Jeong <prichard147@naver.com>
2024-06-05 02:26:41 +0900

    Add user disconnected

--- backend/index.js
+++ backend/index.js
@@ -6,13 +6,18 @@ require("dotenv").config();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:3000"
+    origin: "http://localhost:11634"
   }
 })
 
 // socket에 연결될 사람
 io.on("connection", async (socket) => {
   console.log("client is connected", socket.id);
+
+  // use가 나갔을 경우
+  socket.on("disconnect", () => {
+    console.log("user is disconnected");
+  })
 })
 
 httpServer.listen(process.env.PORT, () => {
