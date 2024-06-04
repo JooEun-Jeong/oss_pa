6d00e8ed9ebf16fb17c3987225ba69aa1b7431ea
JooEun-Jeong <prichard147@naver.com>
2024-06-05 04:22:29 +0900

    Save user data with socket

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const { createServer } = require("http");
 const app = require("./app");
 const { Server } = require("socket.io");
 require("dotenv").config();
+const userController = require("Controllers/user.controller")
 
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
@@ -13,6 +14,19 @@ const io = new Server(httpServer, {
 // socket에 연결될 사람
 io.on("connection", async (socket) => {
   console.log("client is connected", socket.id);
+
+  // 받은 유저의 정보를 저장하고, 소켓 아이디 정보도 저장
+  socket.on("login", async (userName, cb) => {
+    console.log('userName: ' + userName);
+
+    // 유저 정보 저장
+    try {
+      const user = await userController.saveUser(userName, socket.id);
+      cb({ ok: true, data: user });
+    } catch (error) {
+      cb({ ok: false, error: error.message });
+    }
+  })
 
   // use가 나갔을 경우
   socket.on("disconnect", () => {
