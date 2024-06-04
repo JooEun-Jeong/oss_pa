3668e8f0ddc7c0c7f416bfd95f51c767425361a5
JooEun-Jeong <prichard147@naver.com>
2024-06-05 04:22:02 +0900

    Add login function with socket

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,24 @@ import "./App.css";
 import socket from "./server";
 
 function App() {
+  const [user, setUser] = useState(null);
+  const askUserName = () => {
+    const userName = prompt("당신의 이름을 입력하세요");
+    console.log("uuu", userName);
+
+    // emit(대화의 제목, 보낼 내용, 콜백함수)
+    socket.emit("login", userName, (res) => {
+      console.log("Res", res);
+      if (res?.ok) {
+        setUser(res.data);
+      }
+    });
+  }
+
+  useEffect(() => {
+    askUserName();
+  }, [])
+
   return (
     <div>
       <div className="App"></div>
