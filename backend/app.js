5b0613f268a2a1ababdc4b7cf62e1fa6f3f1feb6
JooEun-Jeong <prichard147@naver.com>
2024-06-05 01:49:26 +0900

    Add cors for local connection

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const cors = require('cors');
 const app = express();
+app.use(cors());
 
 mongoose.connect(process.env.DB, {
   useNewUrlParser: true,
