73a767345f0f43eaf6c3a36545e0981acf2bc509
JooEun-Jeong <prichard147@naver.com>
2024-06-05 12:53:53 +0900

    Show messages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,13 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import socket from "./server";
 import InputField from "./components/InputField/InputField";
+import MessageContainer from "./components/MessageContainer/MessageContainer"
 
 function App() {
   const [user, setUser] = useState(null);
   const [message, setMessage] = useState('');
+  const [messageList, setMessageList] = useState([]);
+
   const askUserName = () => {
     const userName = prompt("당신의 이름을 입력하세요");
     console.log("uuu", userName);
@@ -20,16 +23,24 @@ function App() {
   }
 
   useEffect(() => {
+    socket.on('message', (message) => {
+      console.log("Message", message);
+      setMessageList((prev) => prev.concat(message));
+    })
     askUserName();
   }, [])
 
-  const sendMessage = () => {
-
+  const sendMessage = (event) => {
+    event.preventDefault();
+    socket.emit("sendMessage", message, (res) => {
+      console.log("sendMessage", res);
+    })
   }
 
   return (
     <div>
       <div className="App">
+        <MessageContainer message={messageList} user={user} />
         <InputField message={message} setMessage={setMessage} sendMessage={sendMessage} />
       </div>
     </div>
